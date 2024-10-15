da016dc7f102a1d464b3427bd6fa65f09a672b05
srydn <srydn7@gmail.com>
2024-10-15 06:50:15 +0300

    add navbar

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [MatToolbarModule,MatIconModule,MatButtonModule,FormsModule,MatInputModule,MatSelectModule,MatFormFieldModule],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {
+  languages: string[] = ["en","fr","esp"];
+  
+}
